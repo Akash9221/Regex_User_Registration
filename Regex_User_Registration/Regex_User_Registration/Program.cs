556d278eb9bae4b19c61fffe070deec7e53e16f0
“akashmane3938” <maneakash3938@gmail.com>
2022-12-14 21:30:28 -0800

    Check Validation For Last Name

--- Regex_User_Registration/Regex_User_Registration/Program.cs
+++ Regex_User_Registration/Regex_User_Registration/Program.cs
@@ -7,10 +7,16 @@ namespace Regex_User_Registration
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Regular Expression");
-            Console.WriteLine("Enter your first name");
-            string FirstName = Console.ReadLine(); // Creating user input for first name
-            Validation.FirstNameValidation(FirstName);//Calling method to validate firstname
+            //Console.WriteLine("Welcome to Regular Expression");
+            //Console.WriteLine("Enter your first name");
+            //string FirstName = Console.ReadLine(); // Creating user input for first name
+            //Validation.FirstNameValidation(FirstName);//Calling method to validate firstname
+
+           
+            Console.WriteLine("Enter your Last name");
+            string LName = Console.ReadLine(); //  input for last name
+            Validation.LastNameValidation(LName);
+
         }
     }
 }
--- Regex_User_Registration/Regex_User_Registration/UR1.cs
+++ Regex_User_Registration/Regex_User_Registration/UR1.cs
@@ -9,20 +9,20 @@ using System.Threading.Tasks;
 
 namespace Regex_User_Registration
 {
-    internal class Validation
-    {
+    //internal class Validation
+    //{
 
-        public static void FirstNameValidation(string FirstName)
-        {
-            string pattern = "^[A-Z][a-z]{2,}$"; // Creating REGEX pattern
-            if (Regex.IsMatch(FirstName, pattern)) //For checking regex are valid or invalid
-            {
-                Console.WriteLine("The First name is : " + FirstName);
-            }
-            else
-            {
-                Console.WriteLine("Invalid Name : " + FirstName);
-            }
-        }
-    }
+    //    public static void FirstNameValidation(string FirstName)
+    //    {
+    //        string pattern = "^[A-Z][a-z]{2,}$"; // Creating REGEX pattern
+    //        if (Regex.IsMatch(FirstName, pattern)) //For checking regex are valid or invalid
+    //        {
+    //            Console.WriteLine("The First name is : " + FirstName);
+    //        }
+    //        else
+    //        {
+    //            Console.WriteLine("Invalid Name : " + FirstName);
+    //        }
+    //    }
+    //}
 }
